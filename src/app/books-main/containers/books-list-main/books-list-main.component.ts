a648a38598cd5dc64dceb633b72ca5a3bf10d847
DianaD <diana.duque@bizagi.com>
2018-08-09 13:41:30 -0500

    Paginacion

--- src/app/books-main/containers/books-list-main/books-list-main.component.ts
+++ src/app/books-main/containers/books-list-main/books-list-main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookListService } from "../../services/list/book-list.service";
 import { BookList } from "../../models/books";
+import { InterpolationConfig } from '@angular/compiler';
+import { PagerServiceService } from '../../services/pager-service.service'
 
 @Component({
   selector: 'app-books-list-main',
@@ -10,8 +12,10 @@ import { BookList } from "../../models/books";
 export class BooksListMainComponent implements OnInit {
 
   booksList: BookList;
+  pager: any = {};
+  pagedItems: any[];
 
-  constructor(private booksService: BookListService) { 
+  constructor(private booksService: BookListService, private pagerService: PagerServiceService) { 
     this.booksService.searchBooks('Software');
   }
 
@@ -21,9 +25,24 @@ export class BooksListMainComponent implements OnInit {
       (books:BookList) => {
         if(books){
           this.booksList = books;
+          this.setPage(1);
         }        
       }
     );
   }
+
+  
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.booksList.items.length, page);
+
+    // get current page of items
+    this.pagedItems = this.booksList.items.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    console.log(this.pagedItems);
+}
 
 }
